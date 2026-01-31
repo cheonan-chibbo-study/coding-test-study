e355421e7a97c879aadc30b50a9ee4be0c343236
pure77 <72856694+pure77@users.noreply.github.com>
2026-01-31 18:57:14 +0900

    (1주차) 2026.01.31 박민현 문제 풀이 인증 (#12)
    * (1주차)Subsets 풀이 인증
    
    * (1주차) WordSearch 풀이 인증
    
    * (1주차) WordSearch 풀이 인증
    
    * (1주차) WordSearch 풀이 인증
    
    * Complete removal of out directory
    
    * (1주차) BinarySearch,피로도 풀이 인증
    
    * (1주차) 팰린도름,후보키 풀이 인증

---
+++ pmh/week1/PalindromePartitioning/Solution.java
@@ -0,0 +1,38 @@
+package week1.PalindromePartitioning;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        dfs(0,s,new ArrayList<>(),result);
+        return  result;
+    }
+
+    public void dfs(int start,String s,List<String>current,List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            String sub = s.substring(start, i + 1); //이상~미만 자르기
+            if (isPalindrome(sub)) {
+                current.add(sub);
+                dfs(i+1,s,current,result);
+                current.remove(current.size()-1);
+
+            }
+        }
+    }
+
+    boolean isPalindrome(String sub) {
+        int left = 0, right = sub.length() - 1;
+        while (left < right) {
+            if (sub.charAt(left++) != sub.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
