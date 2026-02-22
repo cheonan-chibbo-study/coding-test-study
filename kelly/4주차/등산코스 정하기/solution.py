f9d31c3a1b24cef9d7991361bf5eb675a204469f
kelly6bf <kelly6bf@gmail.com>
2026-02-23 02:07:41 +0900

    (4주차) 등산코스 정하기 코드 수정

--- kelly/4주차/등산코스 정하기/solution.py
+++ kelly/4주차/등산코스 정하기/solution.py
@@ -2,40 +2,37 @@ from collections import defaultdict
 from heapq import heappush, heappop
 
 def solution(n, paths, gates, summits):
+    # 메인 로직
     summits.sort()
-    summit_set = set(summits)
+    summits_set = set(summits)
 
     graph = defaultdict(list)
     for i, j, w in paths:
         graph[i].append((w, j))
         graph[j].append((w, i))
 
-    hq = []
+    pq = []
     visited = [float('inf')] * (n + 1)
-
-	#✅ 모든 출입구를 우선순위큐에 삽입한다.
     for gate in gates:
-        heappush(hq, (0, gate))
+        heappush(pq, (0, gate))
         visited[gate] = 0
 
-	#✅ intensity를 기준으로 다익스트라를 진행한다.
-    while hq:
-        intensity, node = heappop(hq)
-        if intensity > visited[node] or node in summit_set:
+    while pq:
+        cur_inten, cur_v = heappop(pq)
+
+        if cur_inten > visited[cur_v] or cur_v in summits_set:
             continue
 
-        for weight, next_node in graph[node]:
-            next_intensity = max(weight, intensity)
-            if next_intensity < visited[next_node]:
-                #✅ 다익스트라 진행 중 각 노드에 도달하는 과정의 최대 intensity값을 저장한다.
-                visited[next_node] = next_intensity
-                heappush(hq, (next_intensity, next_node))
+        for weight, next_v in graph[cur_v]:
+            next_inten = max(cur_inten, weight)
 
-	#✅ 다익스트라 완료 후 산봉우리들을 순회하며 정답을 찾는다.
-    min_intensity = [0, float('inf')]
-    for summit in summits:
-        if min_intensity[1] > visited[summit]:
-            min_intensity[0] = summit
-            min_intensity[1] = visited[summit]
+            if next_inten < visited[next_v]:
+                visited[next_v] = next_inten
+                heappush(pq, (next_inten, next_v))
 
-    return min_intensity
+    answer = [0, float('inf')]
+    for s in summits:
+        if visited[s] < answer[1]:
+            answer = [s, visited[s]]
+
+    return answer
