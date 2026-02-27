d30896237ca198087d1d8373da5d4f5f1e9eb99c
kelly6bf <kelly6bf@gmail.com>
2026-02-27 21:32:03 +0900

    (5주차) 신규 아이디 추천 풀이 인증

---
+++ kelly/5주차/신규 아이디 추천/solution.py
@@ -0,0 +1,33 @@
+def solution(new_id):
+    # 메인 로직
+
+    # 1단계 (문자를 모두 소문자로 변환)
+    step1 = new_id.lower()
+
+    # 2단계 (주어진 문자열에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거한다.)
+    allowed = {'-', '_', '.'}
+    step2 = ''.join(
+        c for c in step1
+        if c.islower() or c.isdigit() or c in allowed
+    )
+
+    # 3단계 (주어진 문자열에서 처음과 끝에 위치한 마침표(.)를 제거한다.)
+    step3 = step2
+    while '..' in step3:
+        step3 = step3.replace('..', '.')
+
+    # 4단계 (주어진 문자열에서 처음과 끝에 위치한 마침표(.)를 제거한다.)
+    step4 = step3.strip('.')
+
+    # 5단계 (주어진 문자열이 빈 문자열이라면, "a"를 대입한다.)
+    step5 = step4 if step4 else "a"
+
+    # 6단계 (주어진 문자열의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거한다.)
+    step6 = step5[:15].rstrip('.')
+
+    # 7단계 (주어진 문자열의 길이가 2자 이하라면, 마지막 문자를 문자열의 길이가 3이 될 때까지 반복해서 끝에 붙인다.)
+    step7 = step6
+    while len(step7) <= 2:
+        step7 += step7[-1]
+
+    return step7
