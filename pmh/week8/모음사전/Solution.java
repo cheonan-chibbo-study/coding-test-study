295b336711e48cc2a06d27f520c26fbc8a726e3a
pure77 <72856694+pure77@users.noreply.github.com>
2026-03-27 10:17:38 +0900

    (9주차) 모음사전 풀이 인증

---
+++ pmh/week8/모음사전/Solution.java
@@ -0,0 +1,29 @@
+package week8.모음사전;
+class Solution {
+    char[] words = {'A', 'E', 'I', 'O', 'U'};
+    int count = 0;
+    int answer = 0;
+
+    public int solution(String word) {
+        dfs("", word);
+        return answer;
+    }
+
+    public void dfs(String cur, String target) {
+        if (!cur.equals("")) {
+            count++;
+
+            if (cur.equals(target)) {
+                answer = count;
+                return;
+            }
+        }
+
+        if (cur.length() == 5) return;
+
+        for (int i = 0; i < 5; i++) {
+            if (answer != 0) return;
+            dfs(cur + words[i], target);
+        }
+    }
+}
