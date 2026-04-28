a2ff36d5a54427c7157535477550f5b37dd10893
kelly6bf <kelly6bf@gmail.com>
2026-04-28 16:31:56 +0900

    Combinations 수정

--- kelly/완전탐색 & 백트래킹/Combinations/solution02.py
+++ kelly/완전탐색 & 백트래킹/Combinations/solution02.py
@@ -4,5 +4,4 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         # 메인 로직
         candi = [i for i in range(1, n + 1)]
-        combi_list = combinations(candi, k)
-        return [list(combi) for combi in combi_list]
+        return [combi for combi in combinations(candi, k)]
