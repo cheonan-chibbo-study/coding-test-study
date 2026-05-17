40baee34680a23b4209729eaa3ea8b1a73fa1658
pure77 <qkralsgus90@naver.com>
2026-05-17 14:43:44 +0900

    (1주차) 숫자의 표현 풀이 이증

---
+++ pmh/week1/sample5/Solution.java
@@ -0,0 +1,5 @@
+package week1.sample5;
+
+class Solution {
+
+}
---
+++ pmh/week1/숫자의표현/Solution.java
@@ -0,0 +1,27 @@
+package week1.숫자의표현;
+
+class Solution {
+    public int solution(int n) {
+        int sum =0;
+        int count=0;
+        for(int i=1 ;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                sum += j;
+                if(sum == n){
+                    System.out.println(sum);
+                    count++;
+                    sum=0;
+                    break;
+                }else if(sum>n){
+                    sum=0;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+
+
+}
