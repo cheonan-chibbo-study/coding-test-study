b133a5cc9b5c9b9fa5215b05108a3fad8ec1152b
Melai-rgb <k43608569@gmail.com>
2026-07-02 23:12:51 +0900

    숫자의 표현

---
+++ junsik/week07/숫자의 표현/표현.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int left = 1;
+    int right = 1;
+    int sum = 1;
+
+    while(right <= n){
+        if(sum == n){
+            answer++;
+            sum -= left;
+            left++;
+        }
+
+        else if(sum < n){
+            right++;
+            sum += right;
+        }
+        else{
+            sum -= left;
+            left++;
+        }
+    }
+    return answer;
+}
