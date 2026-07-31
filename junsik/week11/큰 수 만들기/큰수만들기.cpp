4fc4df9abfa11dfbd7862fdfe1a98ff169317b51
Melai-rgb <k43608569@gmail.com>
2026-07-31 17:38:44 +0900

    큰 수 만들기 (재풀이)

---
+++ junsik/week11/큰 수 만들기/큰수만들기.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    stack <char> st;
+    
+    for(char c : number){
+        while(!st.empty() && k>0 && st.top() < c){
+            st.pop();
+            k--;
+        }
+        st.push(c);
+    }
+    
+    while(k>0){
+        st.pop();
+        k--;
+    }
+    
+    while(!st.empty()){
+        answer += st.top();
+        st.pop();
+    }
+    
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
